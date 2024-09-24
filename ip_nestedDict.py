5dd62dc40405a727df8cebea21112db45d30f941
plmcdowe <67558494+plmcdowe@users.noreply.github.com>
2024-09-23 23:00:24 -0400

    Add files via upload

---
+++ ip_nestedDict.py
@@ -0,0 +1,7 @@
+#Sanitized and abreviated to give example of structure for the nested IP dictionary 
+remoteSites={
+'SiteName1':[['RTR-IP'], ['SW1-IP', 'SW2-IP']],
+'SiteName2':[['RTR-IP'], ['SW1-IP']],
+'SiteName3':[['RTR-IP'], ['SW1-IP', 'SW2-IP', 'SW3-IP', 'SW4-IP']],
+'SiteName4':[['RTR-IP'], ['SW1-IP']]
+}
